e25c6c34bcf58bccbf4e54b8927e61fdfa25f90f
chrisrichie25 <crl2@williams.edu>
2018-06-25 09:15:57 +0300

    Just before pulling

---
+++ app/src/main/java/apps/inets/com/shulesoft/FirstScreen.java
@@ -0,0 +1,40 @@
+package apps.inets.com.shulesoft;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ * not really used now
+ */
+public class FirstScreen extends Fragment {
+
+
+    public FirstScreen() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.screen_one,container,false);
+
+        /*
+        ViewPager mImageViewPager = (ViewPager) findViewById(R.id.pager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabDots);
+        tabLayout.setupWithViewPager(mImageViewPager, true);
+        */
+
+        return rootView;
+    }
+
+}
