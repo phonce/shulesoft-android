87f88fcd971dec4e1142b923dfca646e11a941ae
chrisrichie25 <crl2@williams.edu>
2018-06-29 13:21:47 +0300

    Home Screen done, left with images and actions on buttons on the toolbar

--- app/src/main/java/apps/inets/com/shulesoft/activities/HomeScreenActivity.java
+++ app/src/main/java/apps/inets/com/shulesoft/activities/HomeScreenActivity.java
@@ -2,25 +2,92 @@ package apps.inets.com.shulesoft.activities;
 
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import apps.inets.com.shulesoft.R;
 
 
 public class HomeScreenActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen);
+        setContentView(R.layout.home_screen_root_view);
 
-        //getting the card views from the home screen layout
-        CardView reportCard =  findViewById(R.id.card_view_reports);
-        CardView paymentCard = findViewById(R.id.card_view_payments);
-        CardView schoolCalendarCard = findViewById(R.id.card_school_calendar);
-        CardView smsAnnouncements = findViewById(R.id.card_sms_announcements);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.the_Toolbar);
 
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        if (navigationView != null) {
+//            setupDrawerContent(navigationView);
+//        }
+//
+//        Button navigationDrawerButton = (Button) findViewById(R.id.navigation_icon);
+//        navigationDrawerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mDrawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
+    }
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+//            case R.id.menu_night_mode_system:
+//                setNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+//                break;
+//            case R.id.menu_night_mode_day:
+//                setNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//                break;
+//            case R.id.menu_night_mode_night:
+//                setNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//                break;
+//            case R.id.menu_night_mode_auto:
+//                setNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
+//                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
 }
--- app/src/main/java/apps/inets/com/shulesoft/activities/MainActivity.java
+++ app/src/main/java/apps/inets/com/shulesoft/activities/MainActivity.java
@@ -1,34 +1,43 @@
 package apps.inets.com.shulesoft.activities;
+
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 
 
 import android.os.Bundle;
 
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import apps.inets.com.shulesoft.R;
-import apps.inets.com.shulesoft.activities.SchoolSearchActivity;
-
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        //startActivity(new Intent(MainActivity.this, FeatureActivity.class));
-
     }
+
 
     public void searchSchools(View view){
         Intent intent = new Intent
                 (this,SchoolSearchActivity.class);
         startActivity(intent);
     }
+
+
 
 }
